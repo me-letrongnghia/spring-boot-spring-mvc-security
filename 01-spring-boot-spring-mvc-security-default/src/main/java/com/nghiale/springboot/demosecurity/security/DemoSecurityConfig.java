addfe84302aa712364d7bb669ad6a12cd91f0290
letrongnghia <nghialee2301@gmail.com>
2024-02-22 16:27:54 +0700

    basic config - add user,pw,role

---
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/nghiale/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.nghiale.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+//B1: Add Configuration Annotation
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    //B2: Create InMemoryUserDetailsManager to save user's password and role
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails nghiale = User.builder()
+                .username("nghiale")
+                .password("{noop}test123")
+                .roles("ADMIN","MANAGER","EMPLOYEE")
+                .build();
+        UserDetails tole = User.builder()
+                .username("tole")
+                .password("{noop}test123")
+                .roles("MANAGER","EMPLOYEE")
+                .build();
+        UserDetails thinhtran = User.builder()
+                .username("thinhtran")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        return new InMemoryUserDetailsManager(nghiale, tole, thinhtran);
+    }
+}
