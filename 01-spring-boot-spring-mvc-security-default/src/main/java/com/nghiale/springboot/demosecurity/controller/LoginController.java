b3fe38e53d32207558f80d47dfe10602b7d3edef
letrongnghia <nghialee2301@gmail.com>
2024-02-25 22:56:31 +0700

    khong biet nen ghi commit la gi nua

--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/nghiale/springboot/demosecurity/controller/LoginController.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/nghiale/springboot/demosecurity/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/loginPage")
     public String showLoginForm() {
-        return "login-form";
+        //return "login-form";
+        return "fancy-login";
     }
 }
